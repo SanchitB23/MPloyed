79b8fb38bd178c2c47ab5e9703fe85e352841b8f
SanchitB23 <sanchitbhatnagar4@gmail.com>
2018-07-29 19:09:05 +0530

    Added Navigation

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StyleSheet, View } from 'react-native';
+import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
+
+import AuthScreen from './src/screens/AuthScreen';
+import WelcomeScreen from './src/screens/WelcomeScreen';
+import MapScreen from './src/screens/MapScreen';
+import DeckScreen from './src/screens/DeckScreen';
+import SettingScreen from './src/screens/SettingScreen';
+import ReviewScreen from './src/screens/ReviewScreen';
 
 export default class App extends React.Component {
   render() {
+    const MainNavigator = createBottomTabNavigator({
+      welcome: WelcomeScreen,
+      auth: AuthScreen,
+      main: createBottomTabNavigator({
+          map: MapScreen,
+          deck: DeckScreen,
+          review: createStackNavigator({
+              review: ReviewScreen,
+              settings: SettingScreen
+            })
+        }, {
+          initialRouteName: 'review'
+        })
+    }, {
+      initialRouteName: 'main'
+    });
+    console.log('kuch hua');
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      // <View>
+        <MainNavigator />
+      // </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
